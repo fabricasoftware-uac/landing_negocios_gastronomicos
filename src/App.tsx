15e85f9a4293888bda0b73f1fdab1f0b1cffbe79
Salomon Montilla <thebilystaxx@gmail.com>
2026-02-17 17:30:55 +0000

    scroll to top ajustado correctamente

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, ScrollRestoration } from "react-router-dom";
 import Index from "./pages/Index";
 import Inscripcion from "./pages/Inscripcion";
 import Reanudar from "./pages/Reanudar";
 import PaymentStatus from "./pages/PaymentStatus";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/inscripcion" element={<Inscripcion />} />
